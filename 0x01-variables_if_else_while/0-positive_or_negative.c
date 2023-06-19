acf52f87a0166d43a6c57f80668ceb1702e5bc0d
aminamohsen <amina.mohsen4004@gmail.com>
2023-06-20 01:02:44 +0300

    posit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,49 +1,29 @@
-# include <stdio.h>
-#include <time.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 /**
- *main - Prints a random number and states whether
- *it is positive, negative, or zero
- * Return: Always 0 (Sucess)
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int n;
-
 	srand(time(0));
-	n = rand() - RAND_Max / 2;
-	double num;
-	    printf("Enter a number: ");
-	        scanf("%lf", &num);
-		    if (num <= 0.0) {
-			            if (num == 0.0)
-					                printf("You entered 0.");
-				            else
-						                printf("You entered a negative number.");
-					        } 
-		        else
-				        printf("You entered a positive number.");
-
-			    return 0;
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	}
+	if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	return (0);
 }
-Run Code
-You can also solve this problem using nested if...else statement.
 
-Check Positive or Negative Using if...else Ladder
-#include <stdio.h>
-
-int main() {
-
-	    double num;
-	        printf("Enter a number: ");
-		    scanf("%lf", &num);
-
-		        if (num < 0.0)
-				        printf("You entered a negative number.");
-			    else if (num > 0.0)
-				            printf("You entered a positive number.");
-			        else
-					        printf("You entered 0.");
-
-				    return (0);
-}
