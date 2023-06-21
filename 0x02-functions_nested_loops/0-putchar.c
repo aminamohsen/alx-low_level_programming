99e35ce1bd2a64d0dfc63b675844890cd9041714
aminamohsen <amina.mohsen4004@gmail.com>
2023-06-21 14:50:57 +0300

    lomy

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,18 @@
-#include <stdio.h>
 #include <main.h>
 /**
  * main - Entry point
- * Description: 'prints _putchar to stdout'
- * Return: always 0
+ * Return: Always 0
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-	return (0);
+	 _putchar('_');
+	 _putchar('p');
+	 _putchar('u');
+	 -putchar('t');
+	 _putchar('c');
+	 _putchar('h');
+	 _putchar('a');
+	 _putchar('r');
+	 _putchar('\n');
+	 return (0);
 }
-
