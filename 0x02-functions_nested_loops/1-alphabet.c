d49e3a105f6c22feeb674c2f4649b91aa5d56b11
aminamohsen <amina.mohsen4004@gmail.com>
2023-06-22 02:26:04 +0300

    reed

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,13 +5,9 @@
  * Return:0
  */
 void print_alphabet(void)
-{
-	 char c = 'a';
+int ch;
 
-	  while (c <= 'z')
-	  {
-		  _putchar(c);
-		  c++;
-	  }
-	  _putchar('\n');
-}			      
+for (ch = 'a'); ch <= 'z'; ch++)
+_putchar(ch);
+_putchar('\n');
+}
