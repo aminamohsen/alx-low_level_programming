b5a9f59db997b513d4f0c090cc2a0a3d98463b58
aminamohsen <amina.mohsen4004@gmail.com>
2023-06-22 00:21:39 +0300

    samy

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <main.h>
+#include <stdio.h>
 /**
  * main - Entry point
  * Return: Always 0
@@ -13,6 +13,6 @@ int main(void)
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
-	_putchar('\n);
+	_putchar('\n');
 	return (0);
 }
