3f8bd571154b52ae5e06af6fa3a75738d2766bd2
aminamohsen <amina.mohsen4004@gmail.com>
2023-06-22 03:47:45 +0300

    Hi

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,10 +2,10 @@
 
 /**
  * _islower - check for lower case letter
- * @c : character to check the case
- * Return:0 or 1
+ * @c: character to check the case
+ * Return: 0 or 1
  */
 int _islower(int c)
 {
-	return (c >= 97 && c <= 122);
+	    return (c >= 97 && c <= 122);
 }
