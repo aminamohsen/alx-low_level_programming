0b2d620a3a69fff5dd1544b2971c4027c02bdd2f
aminamohsen <amina.mohsen4004@gmail.com>
2023-06-22 05:46:30 +0300

    van

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -5,8 +5,7 @@
  * Return:0 or 1
  */
 int _isalpha(int c)
-
-	if ((c >= 65 && c <= 97) || (c >= 97 && c <= 122))
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 {
 	return (1);
 }
@@ -14,4 +13,3 @@ else
 {
 	return (0);
 }
-_putchar('\n');
