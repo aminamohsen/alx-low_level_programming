1ec8915f134616bef88ca1abedb9271bf6af1fb5
aminamohsen <amina.mohsen4004@gmail.com>
2023-08-02 04:18:22 +0300

    fail

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -1,34 +1,40 @@
 #include "lists.h"
-#include <stdlib.h>
 
 /**
- * add_nodeint_end - Add a new node at the end of a list.
- * @head: Address of the first node of a list.
- * @n: Integer to insert in the new node.
- * Return: Address of the new node.
- **/
-
-listint_t *add_nodeint_end(listint_t **head, const int n)
+ * free_listint_safe - frees a linked list
+ * @h: pointer to the first node in the linked list
+ *
+ * Return: number of elements in the freed list
+ */
+size_t free_listint_safe(listint_t **h)
 {
-	listint_t *temp, *temp2;
+	size_t len = 0;
+	int diff;
+	listint_t *temp;
 
-	temp = malloc(sizeof(listint_t));
-	if (temp == NULL)
-		return (NULL);
+	if (!h || !*h)
+		return (0);
 
-	temp->n = n;
-	temp->next = NULL;
-
-	if (*head == NULL)
+	while (*h)
 	{
-		*head = temp;
-		return (temp);
+		diff = *h - (*h)->next;
+		if (diff > 0)
+		{
+			temp = (*h)->next;
+			free(*h);
+			*h = temp;
+			len++;
+		}
+		else
+		{
+			free(*h);
+			*h = NULL;
+			len++;
+			break;
+		}
 	}
 
-	temp2 = *head;
-	while (temp2->next)
-		temp2 = temp2->next;
-	temp2->next = temp;
-	return (temp);
-}
+	*h = NULL;
 
+	return (len);
+}
